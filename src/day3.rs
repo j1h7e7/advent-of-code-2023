624b08b937853ce11dce2ed0ee25bf74f6945fee
j1h7e7 <j1h7e7@gmail.com>
2023-12-02 22:24:45 -0800

    add day 3, part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 fn get_symbol_positions(schematic: &str) -> Vec<Vec<bool>> {
     let h: usize = schematic.lines().count();
     let l: usize = schematic.lines().nth(0).unwrap().chars().count();
@@ -9,8 +11,8 @@ fn get_symbol_positions(schematic: &str) -> Vec<Vec<bool>> {
             if c.is_digit(10) || c == '.' {
                 continue;
             }
-            for x in 0.max(i - 1)..h.min(i + 2) {
-                for y in 0.max(j - 1)..l.min(j + 2) {
+            for x in i.checked_sub(1).unwrap_or(0)..h.min(i + 2) {
+                for y in j.checked_sub(1).unwrap_or(0)..l.min(j + 2) {
                     positions[x][y] = true;
                 }
             }
@@ -45,6 +47,77 @@ fn sum_line_numbers(line: &str, positions: &Vec<bool>) -> i32 {
     return sum;
 }
 
+fn get_potential_gears(schematic: &str) -> Vec<Vec<Vec<(usize, usize)>>> {
+    let h: usize = schematic.lines().count();
+    let l: usize = schematic.lines().nth(0).unwrap().chars().count();
+
+    let mut positions: Vec<Vec<Vec<(usize, usize)>>> = vec![vec![Vec::new(); l]; h];
+
+    for (i, line) in schematic.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            if c != '*' {
+                continue;
+            }
+            for x in i.checked_sub(1).unwrap_or(0)..h.min(i + 2) {
+                for y in j.checked_sub(1).unwrap_or(0)..l.min(j + 2) {
+                    positions[x][y].push((i, j));
+                }
+            }
+        }
+    }
+
+    return positions;
+}
+
+fn get_gear_values(schematic: &str) -> HashMap<(usize, usize), Vec<i32>> {
+    let positions = get_potential_gears(schematic);
+
+    let mut gear_values: HashMap<(usize, usize), Vec<i32>> = HashMap::new();
+    let mut buffer: String = String::new();
+    let mut active_gears: HashSet<(usize, usize)> = HashSet::new();
+
+    fn end_num(
+        buffer: &mut String,
+        active_gears: &mut HashSet<(usize, usize)>,
+        gear_values: &mut HashMap<(usize, usize), Vec<i32>>,
+    ) -> () {
+        if buffer.len() == 0 {
+            return;
+        }
+        let num = buffer.parse::<i32>().unwrap();
+        for gear in active_gears.iter() {
+            gear_values.entry(*gear).or_insert(Vec::new()).push(num);
+        }
+        buffer.clear();
+        active_gears.clear();
+    }
+
+    for (i, line) in schematic.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            if c.is_digit(10) {
+                buffer.push(c);
+                active_gears.extend(positions[i][j].clone());
+            } else {
+                end_num(&mut buffer, &mut active_gears, &mut gear_values);
+            }
+        }
+        end_num(&mut buffer, &mut active_gears, &mut gear_values);
+    }
+    end_num(&mut buffer, &mut active_gears, &mut gear_values);
+    return gear_values;
+}
+
+fn sum_gear_ratios(gear_values: &HashMap<(usize, usize), Vec<i32>>) -> i32 {
+    let mut sum: i32 = 0;
+    for (_, values) in gear_values.iter() {
+        if values.len() != 2 {
+            continue;
+        }
+        sum += values[0] * values[1];
+    }
+    return sum;
+}
+
 pub struct Day3Puzzle {}
 impl super::solve::Puzzle<i32> for Day3Puzzle {
     fn solve(&self, document: &str) -> i32 {
@@ -57,7 +130,7 @@ impl super::solve::Puzzle<i32> for Day3Puzzle {
     }
 
     fn solve2(&self, document: &str) -> i32 {
-        panic!("Not implemented");
+        return sum_gear_ratios(&get_gear_values(document));
     }
 }
 
@@ -106,5 +179,37 @@ mod tests {
         );
         assert_eq!(sum_line_numbers("12.13", &vec![true; 5]), 25);
         assert_eq!(sum_line_numbers("12#13", &vec![true; 5]), 25);
+    }
+
+    #[test]
+    fn test_get_potential_gears() {
+        assert_eq!(
+            get_potential_gears(".*."),
+            vec![vec![vec![(0, 1)], vec![(0, 1)], vec![(0, 1)]]]
+        );
+        assert_eq!(
+            get_potential_gears("*.*"),
+            vec![vec![vec![(0, 0)], vec![(0, 0), (0, 2)], vec![(0, 2)]]]
+        );
+    }
+
+    #[test]
+    fn test_get_gear_values() {
+        assert_eq!(
+            get_gear_values("1*2\n35."),
+            HashMap::from_iter(vec![((0, 1), vec![1, 2, 35])])
+        );
+    }
+
+    #[test]
+    fn test_sum_gear_ratios() {
+        assert_eq!(
+            sum_gear_ratios(&HashMap::from_iter(vec![
+                ((0, 1), vec![1, 2]),
+                ((0, 2), vec![3, 4, 5]),
+                ((1, 1), vec![6]),
+            ])),
+            2
+        );
     }
 }
