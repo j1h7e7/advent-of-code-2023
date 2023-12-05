38efa24631b3a0b67aa8df4a59cc301216148349
j1h7e7 <j1h7e7@gmail.com>
2023-12-04 22:27:24 -0800

    add day 5, part 2

--- src/day5.rs
+++ src/day5.rs
@@ -34,10 +34,10 @@ fn convert_through_maps(maps: &Vec<(i64, i64, i64)>, value: i64) -> i64 {
     return value;
 }
 
-fn find_final_values(document: &str) -> Vec<i64> {
-    let mut chunks = document.split("\n\n");
+fn split_almanac(almanac: &str) -> (Vec<i64>, std::str::Split<'_, &str>) {
+    let mut chunks = almanac.split("\n\n");
 
-    let mut values = chunks
+    let values = chunks
         .next()
         .unwrap()
         .split(':')
@@ -46,6 +46,12 @@ fn find_final_values(document: &str) -> Vec<i64> {
         .split_whitespace()
         .map(|x| x.parse::<i64>().unwrap())
         .collect::<Vec<i64>>();
+
+    return (values, chunks);
+}
+
+fn find_final_values(document: &str) -> Vec<i64> {
+    let (mut values, chunks) = split_almanac(document);
 
     for chunk in chunks {
         let maps = read_maps(chunk);
@@ -56,6 +62,72 @@ fn find_final_values(document: &str) -> Vec<i64> {
     }
 
     return values;
+}
+
+fn convert_range_through_maps(maps: &Vec<(i64, i64, i64)>, range: (i64, i64)) -> Vec<(i64, i64)> {
+    let start = range.0;
+    let end = range.1;
+    let mut ranges: Vec<(i64, i64)> = Vec::new();
+    let mut covered_ranges: Vec<(i64, i64)> = Vec::new();
+
+    for map in maps {
+        // if end < map.0 {
+        //     continue;
+        // }
+        // if start > map.0 + map.2 {
+        //     break;
+        // }
+        let conversion = map.1 - map.0;
+        let low = map.0.max(start);
+        let high = (map.0 + map.2).min(end);
+        if low >= high {
+            continue;
+        }
+        ranges.push((low + conversion, high + conversion));
+        covered_ranges.push((low, high));
+    }
+    covered_ranges.sort();
+
+    let mut extra_ranges: Vec<(i64, i64)> = Vec::new();
+    let mut active = start;
+    for (low, high) in &covered_ranges {
+        if low > &active {
+            extra_ranges.push((active, *low));
+        }
+        active = *high;
+    }
+    if active < end {
+        extra_ranges.push((active, end));
+    }
+
+    ranges.append(&mut extra_ranges);
+    ranges.sort();
+
+    return ranges;
+}
+
+fn find_final_ranges(document: &str) -> Vec<(i64, i64)> {
+    let (values, chunks) = split_almanac(document);
+
+    let mut ranges = values
+        .chunks(2)
+        .map(|x| (x[0], x[0] + x[1]))
+        .collect::<Vec<(i64, i64)>>();
+
+    for chunk in chunks {
+        let maps = read_maps(chunk);
+        let list_ranges = ranges
+            .iter()
+            .map(|x| convert_range_through_maps(&maps, *x))
+            .collect::<Vec<Vec<(i64, i64)>>>();
+        ranges = list_ranges
+            .into_iter()
+            .flatten()
+            .collect::<Vec<(i64, i64)>>();
+    }
+
+    ranges.sort();
+    return ranges;
 }
 
 pub struct Day5Puzzle {}
@@ -69,7 +141,7 @@ impl super::solve::Puzzle<String> for Day5Puzzle {
     }
 
     fn solve2(&self, document: &str) -> String {
-        return String::new();
+        return find_final_ranges(document)[0].0.to_string();
     }
 }
 
@@ -89,5 +161,13 @@ mod tests {
         assert_eq!(convert_through_maps(&vec![(50, 98, 2)], 10), 10);
         assert_eq!(convert_through_maps(&vec![(1, 3, 2), (5, 10, 2)], 6), 11);
         assert_eq!(convert_through_maps(&vec![(1, 3, 2), (5, 10, 2)], 3), 3);
+    }
+
+    #[test]
+    fn test_convert_range_through_maps() {
+        assert_eq!(
+            convert_range_through_maps(&vec![(10, 20, 10), (20, 50, 10)], (0, 30)),
+            vec![(0, 10), (20, 30), (50, 60)]
+        );
     }
 }
