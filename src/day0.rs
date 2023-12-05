1af0159b54f06ee860e6f04d161866a609fd1472
j1h7e7 <j1h7e7@gmail.com>
2023-12-04 22:27:37 -0800

    add template

---
+++ src/day0.rs
@@ -0,0 +1,15 @@
+pub struct Day0Puzzle {}
+impl super::solve::Puzzle<String> for Day0Puzzle {
+    fn solve(&self, document: &str) -> String {
+        panic!("Not implemented");
+    }
+
+    fn solve2(&self, document: &str) -> String {
+        panic!("Not implemented");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
