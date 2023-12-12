50053b812ae1e57665db162311f8d292714d44d7
j1h7e7 <j1h7e7@gmail.com>
2023-12-11 20:29:12 -0800

    add day 9, part 2

--- src/day9.rs
+++ src/day9.rs
@@ -17,12 +17,31 @@ fn get_next_in_sequence(seq: Vec<i64>) -> i64 {
     return ans.iter().sum();
 }
 
-fn get_next_in_sequence_from_line(line: &str) -> i64 {
-    let seq: Vec<i64> = line
-        .split_whitespace()
-        .map(|x| x.parse::<i64>().unwrap())
-        .collect_vec();
-    return get_next_in_sequence(seq);
+fn get_previous_in_sequence(seq: Vec<i64>) -> i64 {
+    let mut ans = Vec::new();
+    let mut cur_seq = seq;
+
+    while !cur_seq.iter().all(|x| *x == 0) {
+        ans.push(cur_seq[0]);
+        let next_seq = cur_seq
+            .iter()
+            .tuple_windows()
+            .map(|(a, b)| b - a)
+            .collect_vec();
+        cur_seq = next_seq;
+    }
+
+    return ans
+        .iter()
+        .enumerate()
+        .map(|(i, x)| {
+            x * match i % 2 {
+                0 => 1,
+                1 => -1,
+                _ => panic!("Invalid index"),
+            }
+        })
+        .sum();
 }
 
 pub struct Day9Puzzle {}
@@ -30,13 +49,27 @@ impl super::solve::Puzzle<String> for Day9Puzzle {
     fn solve(&self, document: &str) -> String {
         return document
             .lines()
-            .map(|x| get_next_in_sequence_from_line(x))
+            .map(|x| {
+                x.split_whitespace()
+                    .map(|y| y.parse::<i64>().unwrap())
+                    .collect_vec()
+            })
+            .map(|x| get_next_in_sequence(x))
             .sum::<i64>()
             .to_string();
     }
 
     fn solve2(&self, document: &str) -> String {
-        panic!("Not implemented");
+        return document
+            .lines()
+            .map(|x| {
+                x.split_whitespace()
+                    .map(|y| y.parse::<i64>().unwrap())
+                    .collect_vec()
+            })
+            .map(|x| get_previous_in_sequence(x))
+            .sum::<i64>()
+            .to_string();
     }
 }
 
@@ -49,5 +82,12 @@ mod tests {
         assert_eq!(get_next_in_sequence(vec![1, 1, 1, 1]), 1);
         assert_eq!(get_next_in_sequence(vec![1, 3, 5, 7]), 9);
         assert_eq!(get_next_in_sequence(vec![1, 3, 6, 10]), 15);
+    }
+
+    #[test]
+    fn test_get_previous_in_sequence() {
+        assert_eq!(get_previous_in_sequence(vec![1, 1, 1, 1]), 1);
+        assert_eq!(get_previous_in_sequence(vec![3, 5, 7, 9]), 1);
+        assert_eq!(get_previous_in_sequence(vec![3, 6, 10, 15]), 1);
     }
 }
